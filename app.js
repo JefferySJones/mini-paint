f3f1518bdd0c9b60f386a2a0d4c17d0bbe44745a
JefferySJones <viability@live.com>
2015-10-14 08:34:39 -0600

    cleaned up app.js and made reset clear history
    removed a lot of console logging.
    in this version and the previous version color change was changed from classes to a .css apply.
    made code more DRY

--- app.js
+++ app.js
@@ -1,113 +1,92 @@
 $(document).ready(function () {
 
-	console.log('I\'m READY!');
-	console.log('I\'m READY!');
-	console.log('I\'m READY!');
+    var color = '#FFFFFF'; //default color white
 
+    var undoArr = [];
 
-	var colors = 'white green red blue yellow'; //used for removing classes
-	var color = 'white'; //default color white
+    //store a limited history in the undoArr
+    function logHistory(that) {
+        if (that.css('background-color') !== color) {
+            if (undoArr.length > 40) {
+                undoArr.shift();
+            }
+            var arr = [];
+            arr.push(that);
+            arr.push(that.css('background-color'));
+            undoArr.push(arr);
+        }
+    }
 
-	var undoArr = [];
+    var drawing = false;
 
-	function logHistory() {
-		if (undoArr.length > 40) {
-			undoArr.shift();
-			console.log('YOUR HISTORY IS GETTING TOO LONG! BALETED!')
-		}
-		console.log('added pixel');
-		var arr = [];
-		arr.push($(this));
-		arr.push($(this).css('background-color'));
-		undoArr.push(arr);
-		console.log(arr);
+    //log history and color
+    function draw(that) {
+        that.css('background-color', color);
+    }
 
-		draw();
-	}
-	
-	//store a history in the undoArr
-	$('.box').on('click', logHistory);
+    //color on click
+    $('.box').on('click', function () {
+        logHistory($(this));
+        draw($(this));
+    });
 
-	var drawing = false;
-	
-	//color
-	$('.box').on('mousedown', function () {
-		drawing = true;
-	});
+    //color on click drag
+    $('.box').on('mouseover', function () {
+        if (drawing) {
+            logHistory($(this));
+            draw($(this));
+        }
+    });
 
-	$('.box').on('mouseup', function () {
-		drawing = false;
-	});
+    $('.box').on('mousedown', function () {
+        drawing = true;
+    });
 
-	$('.box').on('click', function () {
-		$(this).css('background-color', color);
-	});
+    //turn off coloring
+    $('.box').on('mouseup', function () {
+        drawing = false;
+    });
 
-	function draw() {
-		if (drawing) {
-			//$(this).addClass(color);
-			
-			if ($(this).css('background-color') !== color) {
-				if (undoArr.length > 40) {
-					undoArr.shift();
-					console.log('YOUR HISTORY IS GETTING TOO LONG! BALETED!')
-				}
-				console.log('added pixel');
-				var arr = [];
-				arr.push($(this));
-				arr.push($(this).css('background-color'));
-				undoArr.push(arr);
-				console.log(arr);
-			}
-			
-			$(this).css('background-color', color);
-		}
-	}
+    //dblclick erase *note: behaves strange with history
+    $('.box').on('dblclick', function () {
+        $(this).css('background-color', '#000000');
+    });
 
-	$('.box').on('mouseover', draw);
-	
-	//dblclick erase
-	$('.box').on('dblclick', function () {
-		$(this).css('background-color', '#000000');
-	});
-	
-	//undo button
-	$('#undo').on('click', function () {
-		if (undoArr.length) {
-			var element = undoArr[undoArr.length - 1][0]; //Attempting to use the object itself instead of id
-			var elementLastColor = undoArr[undoArr.length - 1][1].toString();
-			console.log(element);
-			console.log(elementLastColor);
-			$(element).css('background-color', elementLastColor);
-			undoArr.pop();
-		} else {
-			console.log('NOTHING TO UNDO');
-		}
-	});
-	
-	//reset button
-	$('#reset').on('click', function () {
-		//$('.box').removeClass(colors);
-		$('.box').css('background-color', '#000000');
-	});
+    //undo button
+    $('#undo').on('click', function () {
+        if (undoArr.length) {
+            var element = undoArr[undoArr.length - 1][0];
+            var elementLastColor = undoArr[undoArr.length - 1][1].toString();
+            $(element).css('background-color', elementLastColor);
+            undoArr.pop();
+        } else {
+            console.log('NOTHING TO UNDO');
+        }
+    });
 
-	$('#red').on('click', function () {
-		color = 'red';
-	});
+    //reset button
+    $('#reset').on('click', function () {
+        $('.box').css('background-color', '#000000');
+        undoArr = [];
+    });
 
-	$('#blue').on('click', function () {
-		color = 'blue';
-	});
+    $('#red').on('click', function () {
+        color = 'red';
+    });
 
-	$('#green').on('click', function () {
-		color = 'green';
-	});
+    $('#blue').on('click', function () {
+        color = 'blue';
+    });
 
-	$('#yellow').on('click', function () {
-		color = 'yellow';
-	});
+    $('#green').on('click', function () {
+        color = 'green';
+    });
 
-	$('#white').on('click', function () {
-		color = 'white';
-	});
+    $('#yellow').on('click', function () {
+        color = 'yellow';
+    });
+
+    $('#white').on('click', function () {
+        color = 'white';
+    });
 });
